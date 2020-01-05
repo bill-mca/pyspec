329abe771ff617fca6e328f3c0ea48edd84c7de2
Bill McAlister <w.mcalister@gmx.com>
2020-01-05 11:26:08 +1100

    Licence and readme updates

--- README.md
+++ README.md
@@ -2,4 +2,8 @@ Pyspec alpha version. Please be carfeful using this code. I've uploaded the
 repository from a version I found on an old thumb drive. I'm Don't think it was
 the final version of the code that I submitted at conclusion of the project. If
 you have a use for this code please contact me at w.mcalister@gmx.com and I'll
-endevour to find the final version or to otherwise help you. 
+endevour to find the final version or to otherwise help you.
+
+Pyspec was developed for an undergraduate project as part of the Summer Scholar
+program at the Australian National University back in 2014. It was written in
+Python 2.7.
--- pyspec.py
+++ pyspec.py
@@ -1,9 +1,29 @@
-# pyspec version 3: pyteomics mod.
+#! /usr/bin/python2
+
 from pyteomics.parser import *
 from pyteomics.mass import *
 from pyteomics.auxiliary import PyteomicsError
 from re import finditer, findall
 
+"""
+Pyspec a python library for analysing crosslink protein digest mass spectrometry
+Copyright (C) 2020  Bill McAlister
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.txt
+
+"""
+# dev ideas:
 # Make an environment object?
 # it could spawn new peptides env.new()
 # it could be called instead of isodistro etc. 
